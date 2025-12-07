b6d400f52571e642c2cc6fbb146ccdb7b805099f
Troplo <troplo@troplo.com>
2025-12-07 17:51:08 +1100

    Update ReceiveNetMessage.cpp

--- src/game/hooks/Network/ReceiveNetMessage.cpp
+++ src/game/hooks/Network/ReceiveNetMessage.cpp
@@ -23,32 +23,35 @@ namespace YimMenu::Hooks
 
 	void Network::ReceiveNetMessage(void* a1, rage::netConnectionManager* mgr, rage::netEvent* event)
 	{
-		LOG(INFO) << "test";
-		if (!event || event->GetEventType() != rage::netEvent::Type::FrameReceived)
-		{
+		if (!g_Running)
 			return BaseHook::Get<Network::ReceiveNetMessage, DetourHook<decltype(&Network::ReceiveNetMessage)>>()->Original()(a1, mgr, event);
-		}
+
+		if (event->GetEventType() != rage::netEvent::Type::FrameReceived)
+			return BaseHook::Get<Network::ReceiveNetMessage, DetourHook<decltype(&Network::ReceiveNetMessage)>>()->Original()(a1, mgr, event);
+
+		// TODO: port security ID stuff from V1
 		auto fr_evt = reinterpret_cast<rage::netEventFrameReceived*>(event);
-		LOG(INFO) << "Receive net message - evt" << fr_evt->m_SecurityId;
 		rage::datBitBuffer buffer(fr_evt->m_Data, fr_evt->m_Length, true);
+
 		rage::netMessage::Type type = ReadType(buffer).value_or(rage::netMessage::Type::Invalid);
-		LOG(INFO) << "Receive net message - evt type" << static_cast<int>(type);
+
 		switch (type)
 		{
 		case rage::netMessage::Type::TextMessage:
 		{
-			LOG(INFO) << "Receive net message - text message";
 			auto player = Players::GetByMessageId(fr_evt->m_MsgId);
 			if (player)
 			{
 				char msg[256]{};
-				buffer.ReadString(msg, sizeof(msg)); // we don't need the rest ChatDisplay::Show(player.GetName(), msg, ImGui::Colors::LightBlue); } return; // the game doesn't handle that anyway } default: break; }
-				return BaseHook::Get<Network::ReceiveNetMessage, DetourHook<decltype(&Network::ReceiveNetMessage)>>()->Original()(a1, mgr, event);
+				buffer.ReadString(msg, sizeof(msg)); // we don't need the rest
+				ChatDisplay::Show(player.GetName(), msg, ImGui::Colors::LightBlue);
 			}
+			return; // the game doesn't handle that anyway
 		}
 		default:
 			break;
 		}
-		return BaseHook::Get<Network::ReceiveNetMessage, DetourHook<decltype(&Network::ReceiveNetMessage)>>()->Original()(a1, mgr, event);
+
+		BaseHook::Get<Network::ReceiveNetMessage, DetourHook<decltype(&Network::ReceiveNetMessage)>>()->Original()(a1, mgr, event);
 	}
 }
